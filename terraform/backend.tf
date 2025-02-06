6d9b8437ded3093e31bd68ab85e6ad46b0eed332
Magnus Kongshem <magnus.kongshem@soprasteria.com>
2025-02-06 14:51:44 +0100

    Terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.76.0"
+      version = "~> 3.90.0"
     }
   }
   backend "azurerm" {
@@ -11,11 +11,14 @@ terraform {
     storage_account_name = "terraformcodeacademy"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    use_oidc             = true
     subscription_id      = "e25e96db-c54e-4251-bed2-5d7a3e52fc4e"
     tenant_id            = "8b87af7d-8647-4dc7-8df4-5f69a2011bb5"
-    client_id            = "939d4d90-44bf-4d8b-9088-32f1c779a788"
   }
 }
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
+
+data "azurerm_client_config" "current" {}
