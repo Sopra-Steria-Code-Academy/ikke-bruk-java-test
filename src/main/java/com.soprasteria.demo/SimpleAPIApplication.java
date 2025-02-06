e0a88cd4fcba696545964e905022943c4d197808
Magnus Kongshem <magnus.kongshem@soprasteria.com>
2025-02-06 14:26:12 +0100

    Deploy

---
+++ src/main/java/com.soprasteria.demo/SimpleAPIApplication.java
@@ -0,0 +1,23 @@
+package com.soprasteria.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+@RequestMapping(value = "/v1/hello")
+@Slf4j
+public class SimpleAPIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleAPIApplication.class, args);
+    }
+
+    @RequestMapping(value = "/world", method = RequestMethod.GET, produces = "text/plain")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello Per Stian!!");
+    }
+}
